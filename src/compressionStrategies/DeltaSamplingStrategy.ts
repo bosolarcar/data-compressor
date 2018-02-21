61d76f56bbce8bf088a95444d6d74d6bb07f9074
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2018-02-21 21:12:03 +0100

    Added Stream interface for deltasampling

--- src/compressionStrategies/DeltaSamplingStrategy.ts
+++ src/compressionStrategies/DeltaSamplingStrategy.ts
@@ -1,23 +1,33 @@
+import * as Rx from "rxjs/Rx";
 import {log} from "../util/Logger";
 import {Math} from "../util/Math";
 
 export class DeltaSamplingStrategy {
+
     public compress(data: number[], delta: number): number[] {
         log.debug("starting segmentation sampling");
         log.debug("Data length: " + data.length);
         log.debug("minimum delta: " + delta);
 
         const output: number[] = [data[0]];
-        let previous: number = data[0];
 
-        data.forEach((element) => {
-            if (Math.absoluteDelta(element, previous) > delta) {
+        let num: number;
+        for (num = 1; num <= data.length; num++) {
+            const previous: number = data[num - 1];
+            const element: number = data[num];
+            if (Math.absoluteDelta(element, previous) > delta){
                 output.push(element);
-                previous = element;
             }
-        });
 
+    }
         return output;
+}
+
+    public compressObservable(data: Rx.Observable<number>, delta: number): Rx.Observable<number> {
+        log.debug("starting segmentation sampling");
+        log.debug("minimum delta: " + delta);
+
+        return data.distinctUntilChanged((x: number, y: number) => Math.absoluteDelta(x, y) < delta);
     }
 
 }
