2bf5161f55e4ad70398b81fcab2392026c328bb8
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2018-03-16 23:47:11 +0100

    Added static levelCompression

--- src/compressionStrategies/LevelCompression.ts
+++ src/compressionStrategies/LevelCompression.ts
@@ -4,8 +4,39 @@ import { ArrayUtil } from "../util/ArrayUtil";
 
 export class LevelCompression {
 
+public compress(data: number[], factor: number): number[][] {
+
+        const bins: number[][] = [];
+        bins[0] = [];
+        log.debug("initialized bin: 0");
+
+        data.forEach((value) => {
+
+            let currentBin: number = 0;
+
+            bins[currentBin].push(value);
+            log.debug("pushed " + value + " to bin: " + currentBin);
+
+            while (bins[currentBin].length % factor === 0) {
+                let avg: number = ArrayUtil.arrayStats(bins[currentBin].slice(bins[currentBin].length - factor, bins[currentBin].length), {avg: true, min: false, max: false, evenArray: false}).avg;
+                currentBin++;
+                if (bins[currentBin] === undefined) {
+                    bins[currentBin] = [];
+                    log.debug("initialized bin: " + currentBin);
+                }
+                bins[currentBin].push(avg);
+                log.debug("pushed " + avg + " to bin: " + currentBin);
+            }
+        });
+        return bins;
+}
+
 //public compress(data: Rx.Observable<number>, factor: number): Rx.Observable<Rx.Observable<number>> {
-public compress(data: Rx.Observable<number>, factor: number): void {
+public compressStream(data: Rx.Observable<number>, factor: number): void {
+
+    let output: Rx.ReplaySubject<number> = new Rx.ReplaySubject(10);
+
+
     let bins: number[][] = [];
     let binNumber: number = 0;
     bins[0] = [];
--- test/LevelCompressionSpec.ts
+++ test/LevelCompressionSpec.ts
@@ -9,9 +9,9 @@ describe("LevelCompression", () => {
                const numbers: number[] = [1, 3, 9, 5, 7, 3, 5, 6, 8];
 
                // Act
-               strategy.compress(Rx.Observable.from(numbers), 3);
+               let result: number[][] = strategy.compress(numbers, 3);
 
                // Assert
-               //expect(result).toEqual([1, 9, 5]);
+               expect(result).toEqual([numbers, [4.3, 5, 6.3], [5.2]]);
            });
    });
