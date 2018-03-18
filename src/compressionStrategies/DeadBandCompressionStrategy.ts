76e1aaf4fce42895d8a1a3d86609b473ec9106db
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2018-03-18 02:23:56 +0100

    Added compression with date to Deadband

--- src/compressionStrategies/DeadBandCompressionStrategy.ts
+++ src/compressionStrategies/DeadBandCompressionStrategy.ts
@@ -1,3 +1,4 @@
+import { DateValuePoint } from "../model/DateValuePoint";
 import {log} from "../util/Logger";
 import {Math} from "../util/Math";
 import { DeadBandOptions } from "./options/DeadBandOptions";
@@ -18,6 +19,25 @@ export class DeadBandCompressionStrategy {
                 }
                 output.push(element);
                 reference = element;
+            }
+    }
+        return output;
+    }
+
+    public compressWithDate(data: DateValuePoint[], opt: DeadBandOptions): DateValuePoint[] {
+        const output: DateValuePoint[] = [data[0]];
+
+        let reference: number = data[0].value;
+        let num: number;
+        for (num = 1; num <= data.length; num++) {
+            const previous: DateValuePoint = data[num - 1];
+            const element: DateValuePoint = data[num];
+            if (Math.absoluteDelta(element.value, reference) > opt.deadBand) {
+                if (opt.sendPrevious) {
+                    output.push(previous);
+                }
+                output.push(element);
+                reference = element.value;
             }
     }
         return output;
