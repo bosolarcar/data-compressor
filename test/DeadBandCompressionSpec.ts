cf4727f09ebacd5a8a8347e27a8261cecabb1d2e
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2018-03-19 00:19:32 +0100

    Added testdata for deadband

--- test/DeadBandCompressionSpec.ts
+++ test/DeadBandCompressionSpec.ts
@@ -1,5 +1,7 @@
 import { DeadBandCompressionStrategy } from "../src/compressionStrategies/DeadBandCompressionStrategy";
 import { DeadBandOptions } from "../src/compressionStrategies/options/DeadBandOptions";
+import { DateValuePoint } from "../src/model/DateValuePoint";
+import { TestDataLoader } from "./visualisation/TestDataUtil";
 
 describe("DeadbandSampling", () => {
 
@@ -31,5 +33,23 @@ describe("DeadbandSampling", () => {
 
         // Assert
         expect(result).toEqual([5, 6, 9, 8, 1]);
+    });
+
+    it("Deadband with date and withou previous", () => {
+
+        const loader: TestDataLoader = new TestDataLoader();
+
+        // Arrange
+        const strategy = new DeadBandCompressionStrategy();
+        const raw = loader.load("temperatur.json");
+
+        const opt: DeadBandOptions = {deadBand: 2, sendPrevious: false, interval: 5};
+
+        // Act
+        const actual = strategy.compressWithDate(raw, opt);
+
+        // Assert
+        const expected = loader.load("deadbandWithoutPrevious.json");
+        expect(actual).toEqual(expected);
     });
 });
