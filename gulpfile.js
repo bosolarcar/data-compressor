5a3d2ac72dd11cbe2c9749cb6cb011aeaddce537
Sebastian Lechte <sebastian.lechte@hs-bochum.de>
2018-03-19 14:55:11 +0100

    added recursive search for typedoc

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var typedoc = require("gulp-typedoc");
 gulp.task("typedoc", function() {
     return gulp
-        .src(["src/*.ts"])
+        .src(["src/**/*.ts"])
         .pipe(typedoc({
             module: "commonjs",
             target: "es6",
